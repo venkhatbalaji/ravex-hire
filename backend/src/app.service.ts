ed87857d6862e8170487cfc11a89123a65709683
Venkhat <117640419+VenkhatBalaji-FE@users.noreply.github.com>
2025-05-21 10:42:13 +0400

    commit

--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-// apps/api/src/app.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World from Ravex API!';
-  }
-}
--- backend/src/auth/guards/jwt-auth.guard.ts
+++
@@ -1,6 +0,0 @@
-// apps/api/src/auth/guards/jwt-auth.guard.ts
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
